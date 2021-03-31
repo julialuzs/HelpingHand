217387b40ea8dc1984619cec4953c8e5ff1875ed
Julia Luz dos Santos <julia.15081@gmail.com>
2021-03-30 23:31:33 -0300

    add initial structure

---
+++ api/src/main/java/com/tcc/helpinghand/HelpinghandApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.helpinghand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpinghandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpinghandApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/tcc/helpinghand/HelpinghandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.helpinghand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpinghandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
