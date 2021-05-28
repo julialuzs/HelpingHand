6422287df312e356b43ac16d4e47a676373d85b6
Julia Luz dos Santos <julia.15081@gmail.com>
2021-05-28 03:17:58 -0300

    feat: add post form

--- app/app/src/main/java/com/tcc/helpinghand/PostActivity.java
+++ app/app/src/main/java/com/tcc/helpinghand/PostActivity.java
@@ -2,13 +2,35 @@ package com.tcc.helpinghand;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PostActivity extends AppCompatActivity {
+
+    private EditText etPostBody;
+
+    private Button btSave;
+    private Button btCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        this.initializeComponents();
+
+        this.btCancel.setOnClickListener(view -> {
+            Intent intent = new Intent();
+            startActivity(intent);
+        });
+    }
+
+    private void initializeComponents() {
+        this.etPostBody = findViewById(R.id.et_post_body);
+        this.btCancel = findViewById(R.id.bt_post_back);
+        this.btSave = findViewById(R.id.bt_post_save);
     }
 }
